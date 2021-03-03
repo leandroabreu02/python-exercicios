5a6105dda512186dc7e12ad8f6e030ef187b5f6e
leandroabreu13 <djleandro.cardoso@gmail.com>
2021-03-03 13:25:43 -0300

    ex083

--- ex078.py
+++ ex078.py
@@ -1,11 +1,25 @@
 # Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
 # No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista
-numero1 = int(input('Primeiro número: '))
-numero2 = int(input('Segundo número: '))
-numero3 = int(input('Terceiro número: '))
-numero4 = int(input('Quarto número: '))
-numero5 = int(input('Quinto número: '))
-valores = [numero1, numero2, numero3, numero4, numero5]
-print(f'Você digitou os valores {valores}')
-print(f'O maior valor digitado foi {valores} nas posições {valores}')
-print(f'O menor valor digitado foi {valores} nas posições {valores}')
+numeros = []
+maior = 0
+menor = 0
+for c in range(0,5):
+  numeros.append(int(input(f'Digite um valor para a posição {c}: ')))
+  if c == 0:
+    maior = menor = numeros[c]
+  else:
+    if numeros[c] > maior:
+      maior = numeros[c]
+    if numeros[c] < menor:
+      menor = numeros[c]
+print(f'Você digitou os valores {numeros}')
+print(f'O maior valor digitado foi {maior} nas posições ', end='')
+for i, v in enumerate(numeros):
+  if v == maior:
+    print(f'{i}...', end='')
+print()
+print(f'O menor valor digitado oif {menor} nas posições ', end='')
+for i, v in enumerate(numeros):
+  if v == menor:
+    print(f'{i}...', end='')
+print()
--- ex081.py
+++ ex081.py
@@ -3,3 +3,15 @@
 # A) Quantos números foram digitados
 # B) A lista de valores, ordenada de forma decrescente
 # C) Se o valor 5 foi digitado e está ou não na lista
+valores = []
+while True:
+  valores.append(int(input('Digite um valor: ')))
+  resp = str(input('Quer continuar? [S/N] '))
+  if resp in 'Nn':
+    break
+print(f'Você digitou {len(valores)} elementos')
+if 5 in valores:
+  print('O valor 5 faz parte da lista')
+else:
+  print('O valor 5 não foi encontrado na lista')
+  
